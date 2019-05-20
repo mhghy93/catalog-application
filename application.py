7bfb519c33381246914c334ad56aaf147677abb7
mhghy93 <hussainmustaque98407@gmail>
2019-05-21 01:55:51 +0530

    correct port number

--- application.py
+++ application.py
@@ -281,4 +281,4 @@ def deleteCatalog(catalog_id):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
